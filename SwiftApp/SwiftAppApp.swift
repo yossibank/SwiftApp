47f876cc1baa04ecb6f11d4f1f6e4b52e602adad
yossibank <yossibank@yahoo.co.jp>
2023-09-23 11:17:33 +0900

    create project

---
+++ SwiftApp/SwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAppApp.swift
+//  SwiftApp
+//
+//  Created by Kamiyama Yoshihito on 2023/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
