e712a891de00e19f3f1a46abdbf293b16c499b7b
yossibank <yossibank@yahoo.co.jp>
2023-10-08 15:45:21 +0900

    create build function

--- Macro/StructBuilderMacro/Sources/StructBuilderMacro/StructBuilderMacro.swift
+++ Macro/StructBuilderMacro/Sources/StructBuilderMacro/StructBuilderMacro.swift
@@ -31,21 +31,61 @@ public struct CustomBuilderMacro: PeerMacro {
         providingPeersOf declaration: some DeclSyntaxProtocol,
         in context: some MacroExpansionContext
     ) throws -> [DeclSyntax] {
-        guard let structDeclration = declaration.as(StructDeclSyntax.self) else {
+        // 木構造はここで調べながら検証
+        // https://swift-ast-explorer.com/
+        guard let structDeclaration = declaration.as(StructDeclSyntax.self) else {
             return []
         }
 
-        return ["""
-        struct PersonBuilder {
-            var name: String = ""
+        let members = structDeclaration.memberBlock.members
+            .compactMap { $0.decl.as(VariableDeclSyntax.self)?.bindings.first }
+            .compactMap { $0.pattern.as(IdentifierPatternSyntax.self) }
 
-            func build() -> Person {
-                return Person(
-                    name: name
+        let name = TokenSyntax
+            .identifier(structDeclaration.name.text + "Builder")
+            .with(\.trailingTrivia, .spaces(1))
+
+        var returnStatement = ReturnStmtSyntax()
+        returnStatement.expression = ExprSyntax(
+            FunctionCallExprSyntax(
+                calledExpression: DeclReferenceExprSyntax(baseName: structDeclaration.name.trimmed),
+                leftParen: .leftParenToken(trailingTrivia: .newline.appending(Trivia.spaces(4))),
+                arguments: LabeledExprListSyntax(
+                    members.map { member in
+                        LabeledExprSyntax(
+                            label: member.identifier.text,
+                            expression: ExprSyntax(stringLiteral: member.identifier.text)
+                        )
+                    }
+                ),
+                rightParen: .rightParenToken(leadingTrivia: .newline)
+            )
+        )
+
+        let buildFunction = FunctionDeclSyntax(
+            name: .identifier("build"),
+            signature: FunctionSignatureSyntax(
+                parameterClause: FunctionParameterClauseSyntax(parameters: FunctionParameterListSyntax([])),
+                returnClause: ReturnClauseSyntax(type: TypeSyntax(stringLiteral: structDeclaration.name.text))
+            )
+        ) {
+            CodeBlockItemListSyntax([
+                CodeBlockItemListSyntax.Element(
+                    item: CodeBlockItemListSyntax.Element.Item.stmt(StmtSyntax(returnStatement))
                 )
-            }
+            ])
         }
-        """]
+
+        let structureDeclaration = StructDeclSyntax(
+            name: name,
+            memberBlockBuilder: {
+                MemberBlockItemListSyntax([
+                    MemberBlockItemListSyntax.Element(decl: buildFunction)
+                ])
+            }
+        )
+
+        return [DeclSyntax(structureDeclaration)]
     }
 }
 
