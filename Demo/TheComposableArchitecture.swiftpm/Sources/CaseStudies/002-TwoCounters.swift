d15b44c83f3a2c5ede9ce887657d4df7568c3aef
yossibank <yossibank@yahoo.co.jp>
2023-12-09 11:22:44 +0900

    add test in TCA

--- Demo/TheComposableArchitecture.swiftpm/Sources/CaseStudies/002-TwoCounters.swift
+++ Demo/TheComposableArchitecture.swiftpm/Sources/CaseStudies/002-TwoCounters.swift
@@ -79,3 +79,26 @@ struct TwoCountersView: View {
         TwoCountersView()
     }
 }
+
+// MARK: - Test
+
+// import XCTest
+//
+// @MainActor
+// final class TwoCountersTests: XCTestCase {
+//    func testCount() async {
+//        let store = TestStore(initialState: TwoCounters.State()) {
+//            TwoCounters()
+//        }
+//
+//        await store.send(.counter1(.incrementButtonTapped)) {
+//            $0.counter1.count = 1
+//            $0.counter2.count = 0
+//        }
+//
+//        await store.send(.counter2(.decrementButtonTapped)) {
+//            $0.counter1.count = 1
+//            $0.counter2.count = -1
+//        }
+//    }
+// }
